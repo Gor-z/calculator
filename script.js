904a26f9e4ab79e986fc1bdd8123f30b51a11320
Gor-z <211397625+Gor-z@users.noreply.github.com>
2025-06-24 18:36:45 -0300

    add ui, display and operate functions

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const digits = document.querySelectorAll('.digit')
+const display = document.querySelector('#calculator-display')
+const operators = document.querySelectorAll('.operator')
+const clear = document.querySelector('#clear')
+const equals = document.querySelector('#equals')
+
+
+let a = ''
+let b = ''
+let mainOperator = ''
+let secundaryOperator = ''
+
 const add = (a, b) => a + b
 const subtract = (a, b) => a - b
 const multiply = (a, b) => a * b
@@ -6,18 +18,66 @@ const divide = (a, b) => b != 0 ? a / b : 'error'
 const operate = (a, b, operator) => {
     switch (operator) {
         case '+':
-            add(a, b)
-            break;
+            return add(a, b)
         case '-':
-            subtract(a, b)
-            break;
+            return subtract(a, b)
         case '*':
-            multiply(a, b)
-            break;
+            return multiply(a, b)
         case '/':
-            divide(a, b)
-            break;
+            return divide(a, b)
         default:
             break;
     }
 }
+
+const updateDisplay = () => {
+    display.textContent = `${a} ${mainOperator} ${b} ${secundaryOperator}`
+}
+
+const clearDisplay = () => {
+    a = ''
+    b = ''
+    mainOperator = ''
+    secundaryOperator = ''
+    updateDisplay()
+}
+
+digits.forEach((digit) => {
+    digit.addEventListener('click', () => {
+        if (mainOperator == '') {
+        a += digit.textContent
+        }else if(secundaryOperator == ''){
+        b += digit.textContent
+        }
+        updateDisplay()
+    })
+})
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        if (a != '' && mainOperator == '') {
+            mainOperator = operator.textContent.trim()
+            updateDisplay()
+        }else if (mainOperator != '' && b != '' && secundaryOperator == '') {
+            secundaryOperator = operator.textContent
+            updateDisplay()
+        }
+    })
+    
+})
+
+equals.addEventListener('click', () => {
+    let result 
+    result = operate(Number(a), Number(b), mainOperator)
+    a = result
+    b = ''
+    mainOperator = ''
+    if (secundaryOperator != '') {
+        mainOperator = secundaryOperator
+    }
+    secundaryOperator = ''
+    updateDisplay()
+})
+
+clear.addEventListener('click', clearDisplay)
+
